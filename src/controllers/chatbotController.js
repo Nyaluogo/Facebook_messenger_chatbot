7f5bc38f08e1fe7dc92e28188b47b8fd4bfcdfe2
edwin nyaluogo <edwinnyaluogo@gmail.com>
2020-10-13 09:44:33 +0300

    update6

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -83,7 +83,7 @@ function handleMessage(sender_psid, received_message) {
     // will be added to the body of our request to the Send API
     response = {
       // "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-      "text": `Hi, "${sender_psid.name}". My name is Edwin, Nyabingi Studio founder. I'm an indie video game developer and a concept artist. Here are some of my work.`,
+      "text": `Hi, My name is Edwin, Nyabingi Studio founder. I'm an indie video game developer and a concept artist. Here are some of my work.`,
       "attachment": {
         "type": "template",
         "payload": {
